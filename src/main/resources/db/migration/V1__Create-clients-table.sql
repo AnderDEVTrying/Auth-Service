a32b23a3e9f9598e175ed8bf76e78a95d583451a
Anderson Zango <azango94@gmail.com>
2024-10-28 13:32:58 +0200

    Creating clients migration

---
+++ src/main/resources/db/migration/V1__Create-clients-table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE customers  (
+    id SERIAL PRIMARY KEY,
+    userName VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(50) NOT NULL
+);
